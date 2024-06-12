00e4854f1f1936d8384fc8bf7db2e38fc0478843
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-12 16:03:55 -0600

    ✨ feat: contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  formContact: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.formContact = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      comments: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  hasErrors = (controlName: string, errorType: string) =>
+    this.formContact.get(controlName)?.hasError(errorType) && this.formContact.get(controlName)?.touched;
+
+  isValid = (controlName: string) =>
+    this.formContact.get(controlName)?.valid && this.formContact.get(controlName)?.touched;
+
+  sendForm = (event: Event) => {
+    if (this.formContact.invalid) {
+      this.formContact.markAllAsTouched();
+      return;
+    }
+    console.log(this.formContact);
+  };
 }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { CurrencyPipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, TitleCasePipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent implements OnInit {
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+
+  loading: boolean = true;
+  product?: IProduct;
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params) => {
+      this._apiService.getProduct(params['id']).subscribe((product: IProduct) => {
+        this.loading = false;
+        this.product = product;
+        console.log(product);
+      });
+    });
+  }
+}
