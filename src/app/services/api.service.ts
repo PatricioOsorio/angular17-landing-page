5ae38cc7e175cc0ea7e3643b1f6252bfc18ddf3f
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-12 14:39:08 -0600

    ✨ feat: services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private _http = inject(HttpClient);
+  private _urlBase = 'https://fakestoreapi.com/products';
 
-  constructor() { }
+  getProducts(): Observable<IProduct[]> {
+    return this._http.get<IProduct[]>(this._urlBase);
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this._http.get<IProduct>(`${this._urlBase}/${id}`);
+  }
 }
