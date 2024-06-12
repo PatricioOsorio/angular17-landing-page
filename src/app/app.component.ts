a25d9227b3039db31d4fb481cdbf76653066285e
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-12 14:40:30 -0600

    ✨ feat: product page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular17-landing-page';
+
+  isActive(url: string): boolean {
+    return window.location.pathname === url; 
+  }
+
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  private _apiService = inject(ApiService);
+  private _router = inject(Router);
 
+  products: IProduct[] = [];
+
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data: IProduct[]) => {
+      this.products = data;
+    });
+  }
+
+  navigate(id: number): void {
+    this._router.navigate(['/products', id])
+  }
 }
